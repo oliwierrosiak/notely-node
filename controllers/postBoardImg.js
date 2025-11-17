3123e9ab51d1ed59100bd2e8aaf16897e4483200
Oliwier <oliwierrosiak07@gmail.com>
2025-11-17 23:02:45 +0100

    Foto upload to aws added

--- controllers/postBoardImg.js
+++ controllers/postBoardImg.js
@@ -1,7 +1,17 @@
-function postBoardImg(req,res)
+import s3Upload from "../s3/s3Upload.js"
+
+async function postBoardImg(req,res)
 {
-    console.log(req.file)
-    res.sendStatus(200)
+    try
+    {
+        await s3Upload(`${req.file.destination}${req.file.filename}`,`boardImg/${req.file.filename}`,req.file.mimetype)
+        const link = `https://interactive-board-storage.s3.eu-north-1.amazonaws.com/boardImg/${req.file.filename}`
+        res.status(200).json({link})
+    }
+    catch(ex)
+    {
+        res.sendStatus(500)
+    }
 }
 
 export default postBoardImg
---
+++ s3/s3Upload.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import { PutObjectCommand } from '@aws-sdk/client-s3'
+import s3 from './s3Config.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+function s3Upload(file,name,mimetype)
+{
+    const buffer = fs.readFileSync(file)
+    const command = new PutObjectCommand({
+        Bucket:process.env.AWS_BUCKET_NAME,
+        Key:name,
+        Body:buffer,
+        ContentType:mimetype
+    })
+
+    return s3.send(command)
+
+}
+
+export default s3Upload
