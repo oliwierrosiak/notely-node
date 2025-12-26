71f1c1cbe408d04ce53617c7dde03b5f0eb6c336
Oliwier <oliwierrosiak07@gmail.com>
2025-12-26 22:22:42 +0100

    Added support to deleting board items

---
+++ controllers/deleteBoardItem.js
@@ -0,0 +1,33 @@
+import { Notes } from "../db/dbConfig.js"
+
+async function deleteBoardItem(req,res)
+{
+    try
+    {
+        const note = await Notes.findOne({_id:req.params.boardId})
+        if(!note)
+        {
+            throw new Error()
+        }
+        const elements = [...note.content]
+        const index = elements.findIndex(x=>x.id === req.params.elementId)
+        if(index === -1)
+        {
+            throw new Error()
+        }
+
+        elements.splice(index,1)
+
+        note.content = elements
+
+        await note.save()
+
+        res.sendStatus(200)
+    }
+    catch(ex)
+    {
+        res.sendStatus(500)
+    }
+}   
+
+export default deleteBoardItem
--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,9 @@ import express from 'express'
 import postBoardImg from '../controllers/postBoardImg.js'
 import boardMediaUpload from '../middleware/uploadBoardMedia.js'
 import getBoardData from '../controllers/getBoardData.js'
-import updateBoard from '../controllers/updateNoteTitle.js'
+import updateBoardTitle from '../controllers/updateNoteTitle.js'
 import updateNoteContent from '../controllers/updateNoteContent.js'
+import deleteBoardItem from '../controllers/deleteBoardItem.js'
 
 const Router = new express.Router()
 
@@ -11,8 +12,10 @@ Router.post('/boardImg',boardMediaUpload.single('img'),postBoardImg)
 
 Router.get('/getBoardData/:id',getBoardData)
 
-Router.post('/updateNoteTitle/:id',updateBoard)
+Router.post('/updateNoteTitle/:id',updateBoardTitle)
 
 Router.post('/updateNoteContent/:id',updateNoteContent)
+
+Router.delete('/deleteBoardItem/:boardId/:elementId',deleteBoardItem)
 
 export default Router
